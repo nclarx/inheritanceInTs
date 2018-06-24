a02dbc4e40b8057dd47a25ba34aac38ef8001104
NClarx <nick@clarx.com.au>
2018-06-24 22:03:22 +1000

    Changed generate methods in FakerService to static methods and moved instancing of class objects inside those methods. Added some types to methods in FakerService.

--- src/app/faker.service.ts
+++ src/app/faker.service.ts
@@ -1,31 +1,15 @@
 import {Injectable} from '@angular/core';
 import * as faker from 'faker';
-import {IPerson, Person} from "./entities/person";
-import {Teacher} from "./entities/teacher";
+import {IPerson, Person} from './entities/person';
+import {ITeacher, Teacher} from './entities/teacher';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FakerService {
-
-  numberOfPeopleToGenerate: number = 10;
-  peopleList: Array<IPerson> = [];
-  teacherList: Array<any> = [];
-
-  constructor() {
-    for (let i = 0; i < this.numberOfPeopleToGenerate; i++) {
-      this.peopleList.push(new Person(this.generatePerson()));
-      this.teacherList.push(new Teacher(this.generateTeacher()));
-    }
-    console.groupCollapsed('Generated Data ====================');
-    console.log('People List: ', this.peopleList);
-    console.log('Teacher List: ', this.teacherList);
-    console.groupEnd();
-  }
-
-  generatePerson(): IPerson {
-    return {
-      id: undefined, // faker.random.uuid(),
+  static generatePerson(): IPerson {
+    return new Person({
+      id: faker.random.uuid(),
       type: 'Person',
 
       firstName: faker.name.firstName(),
@@ -37,11 +21,11 @@ export class FakerService {
 
       dateAdded: faker.date.past(),
       dateModified: faker.date.past()
-    }
+    });
   }
 
-  generateTeacher(): any {
-    return {
+  static generateTeacher(): ITeacher {
+    return new Teacher({
       id: faker.random.uuid(),
       type: 'Teacher',
 
@@ -57,6 +41,21 @@ export class FakerService {
 
       dateAdded: faker.date.past(),
       dateModified: faker.date.past()
+    });
+  }
+
+  numberOfPeopleToGenerate: number = 10;
+  peopleList: Array<IPerson> = [];
+  teacherList: Array<ITeacher> = [];
+
+  constructor() {
+    for (let i = 0; i < this.numberOfPeopleToGenerate; i++) {
+      this.peopleList.push(FakerService.generatePerson());
+      this.teacherList.push(FakerService.generateTeacher());
     }
+    console.groupCollapsed('Generated Data ====================');
+    console.log('People List: ', this.peopleList);
+    console.log('Teacher List: ', this.teacherList);
+    console.groupEnd();
   }
 }
